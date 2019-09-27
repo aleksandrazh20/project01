c13fb7852cfaa1b40df09af0fa612034311ce871
aleksandrazh20 <hollandjoe20@gmail.com>
2019-09-27 21:53:32 +0700

    init

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+# Automatic probe "Voyager-1", exploring the solar system and its surroundings launched on Sep 5. 1977
+#  Sep 25 2009 Voyager-1 was at a distance of 16.637 billion miles from the sun and was traveling at a speed of 38,241 miles / hour.
+#  The user enters the number of days from September 25, the program considers the distance in miles, km and the astronomer. units
+# # Calculate the delay of wireless communication at a wave propagation speed of 299792458 m / h
 days = int(input())
-distml = 38241
-newdistml = distml*24*days
+speed = 38241
+newdistml = 16637000000 + speed*24*days
 newdistkm = newdistml*1.61
 newdistau = newdistkm/1.496e+8
-
+svspeed = 299792458*3.6
+sv = newdistkm/svspeed
 print(newdistml)
 print(newdistkm)
 print(newdistau)
+print(sv)
 
