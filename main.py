b966577c3ba9fc37cea85f177cd8c1032faacdea
aleksandrazh20 <hollandjoe20@gmail.com>
2019-09-27 09:54:11 +0700

    init

---
+++ main.py
@@ -0,0 +1,10 @@
+days = int(input())
+distml = 38241
+newdistml = distml*24*days
+newdistkm = newdistml*1.61
+newdistau = newdistkm/1.496e+8
+
+print(newdistml)
+print(newdistkm)
+print(newdistau)
+
